67494a313f78c8574e9fab0fc369ccf2f70e9f1d
GAURAV SHARMA <108004723+gauravrdj@users.noreply.github.com>
2024-10-28 18:41:15 +0530

    yuvraj Sire

--- frontend/src/components/Foot.jsx
+++ frontend/src/components/Foot.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <p className="text-xl font-semibold">Lodu Yuvraj @NIT Rourkela</p>
+        <p className="text-xl font-semibold">Yuvraj Prajapat @NIT Rourkela</p>
         <div>
           <a href="http://www.linkedin.com/in/pramod-yadav-418695268" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">My Linkedin</a>
           {/* <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Link 2</a>
